def82e1688877a60770a80338dbdd3709fd0670f
Taurean Wooley <taurean.wooley@gmail.com>
2018-06-19 19:41:33 -0700

    Deleting Sensitive Information

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	DEFINE('LOCALHOST', 'ekoprisedb.db.9237977.hostedresource.com');
-	DEFINE('DB_USERNAME', 'ekoprisedb');
-	DEFINE('DB_PASSWORD', 'Ntisman1!');
-	DEFINE('DB_NAME', 'ekoprisedb');
-	DEFINE('DEBUG', 1);
-?>
